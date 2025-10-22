21c6e62854fdfff571191128dcadb17d4e87ab9f
Vini-Araujoo <241025425@aluno.unb.br>
2025-10-21 21:36:48 -0300

    Subindo Back-end

---
+++ Back/olimpo/src/main/java/com/olimpo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.olimpo.repository;
+
+import com.olimpo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
