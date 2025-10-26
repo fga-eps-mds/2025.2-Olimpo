0dbc8df89136cda8eec65f91c6d2bdfcfa8ed9d2
Vini-Araujoo <241025425@aluno.unb.br>
2025-10-26 16:51:41 -0300

    feat: Configuração do bean para hashing e atualização da classe User para Account com colunas atualizadas

---
+++ Back/olimpo/src/main/java/com/olimpo/models/Account.java
@@ -0,0 +1,69 @@
+package com.olimpo.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+
+@Entity // 1. Diz ao JPA: esta classe representa uma tabela.
+@Table(name = "ACCOUNT") // 2. Liga explicitamente esta classe à tabela "ACCOUNT" do SQL.
+@Data // Lombok: Gera getters, setters, toString, equals, hashCode (Opcional)
+@NoArgsConstructor // Lombok: Gera o construtor vazio OBRIGATÓRIO para o JPA.
+@AllArgsConstructor // Lombok: Gera um construtor com todos os campos (útil para criar objetos).
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "is_email_verified")
+    private boolean emailVerified = false;
+
+    @Column(name = "pfp", length = 512)
+    private String pfp;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @Column(name = "estado")
+    private String estado;
+
+    @Column(name = "faculdade")
+    private String faculdade;
+
+    @Column(name = "semestre")
+    private Integer semestre;
+
+    @Column(name = "curso")
+    private String curso;
+
+    @Column(name = "doc_type", nullable = false)
+    private String docType;
+
+    @Column(name = "doc_number", nullable = false, unique = true)
+    private String docNumber;
+
+    @Column(name = "bio", columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(name = "created_at", updatable = false)
+    private OffsetDateTime createdAt;
+
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+    }
+
+}
+
--- Back/olimpo/src/main/java/com/olimpo/service/UserService.java
+++ Back/olimpo/src/main/java/com/olimpo/service/UserService.java
@@ -1,15 +1,18 @@
 package com.olimpo.service;
 
 import com.olimpo.models.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.olimpo.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     public User create(String username, String password){
         User user = new User();
